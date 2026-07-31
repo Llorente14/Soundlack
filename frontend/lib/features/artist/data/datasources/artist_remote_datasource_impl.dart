7f2f3390a840fb33a1921eb5e527b5c7e6fe357c
Llorente14 <axel.crisdy1980@gmail.com>
2026-07-31 17:43:38 +0700

    fix: handle Dio exceptions in getArtist method remoteDatasource for better error management

--- frontend/lib/features/artist/data/datasources/artist_remote_datasource_impl.dart
+++ frontend/lib/features/artist/data/datasources/artist_remote_datasource_impl.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:frontend/core/error/exception.dart';
 import 'package:frontend/features/artist/data/datasources/artist_remote_datasource.dart';
 import 'package:frontend/features/artist/data/models/artist_response_model.dart';
 
@@ -9,12 +10,21 @@ class ArtistRemoteDatasourceImpl implements ArtistRemoteDataSource {
 
   @override
   Future<List<ArtistResponseModel>> getArtist() async {
-    final response = await dio.get('/artist');
-    final Map<String, dynamic> data = response.data;
-    final List<dynamic> body = data['body'];
+    try {
+      final response = await dio.get('/artist');
+      final Map<String, dynamic> data = response.data;
+      final List<dynamic> body = data['body'];
 
-    return body
-        .map((artistJson) => ArtistResponseModel.fromJson(artistJson))
-        .toList();
+      return body
+          .map((artistJson) => ArtistResponseModel.fromJson(artistJson))
+          .toList();
+    } on DioException catch (e) {
+      if (e.type == DioExceptionType.connectionTimeout ||
+          e.type == DioExceptionType.connectionError) {
+        throw NetworkException();
+      } else {
+        throw ServerException();
+      }
+    }
   }
 }
